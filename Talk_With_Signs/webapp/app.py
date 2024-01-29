88939645512d5a8c70b0984edcb89f2c76065c1c
VISHNU PRASAD <132830156+vishnuprasad5@users.noreply.github.com>
2024-01-29 18:34:20 +0530

    Update app.py

--- Talk_With_Signs/webapp/app.py
+++ Talk_With_Signs/webapp/app.py
@@ -114,7 +114,7 @@ def predict_letters(results, frame):
                 normalized_hand_landmarks_flat = normalized_hand_landmarks_flat.reshape(1, -1)
 
                 class_label = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
-                               10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'E', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
+                               10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
                                19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z', 26: 'EOW', 27: 'EOS'}
 
                 prediction = loaded_model.predict(normalized_hand_landmarks_flat)
